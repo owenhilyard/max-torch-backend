08fbf324060113beebf049ddc52da0164fab19b4
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2025-08-05 18:50:03 +0000

    It works kinda

--- main.py
+++ main.py
@@ -1,31 +1,69 @@
 
 import torch
 import max
-from max.torch import graph_op
+from max.torch.torch import graph_op, MaxOp, CustomOpLibrary
+import inspect
+from max.graph import KernelLibrary
+from max import mlir
 
 print("imports done")
 
-def max_add(a, b):
-    return a + b
 
-def max_add_i_want_to_use(*args):
-    return args[0] + args[1]
+class MyMaxOp(MaxOp):
+    def __init__(self, *args, num_inputs: int, **kwargs):
+        self.num_inputs = num_inputs
+        super().__init__(*args, **kwargs)
+     
+    @property
+    def torch_signature(self) -> inspect.Signature:
+        dps_args = [
+            inspect.Parameter(
+                f"__out{i}",
+                inspect.Parameter.POSITIONAL_OR_KEYWORD,
+                annotation=torch.Tensor,
+            )
+            for i in range(self.num_outputs)
+        ]
+        args = [
+            inspect.Parameter(
+                f"__input{i}",
+                inspect.Parameter.POSITIONAL_OR_KEYWORD,
+                annotation=torch.Tensor,
+            )
+            for i in range(self.num_inputs)
+        ]
+        return inspect.Signature((*dps_args, *args), return_annotation=None)
 
-max_add_graph_op = graph_op(max_add)
-
-
-def torch_add_with_max(*args) -> torch.Tensor:
-    result = args[0].new_empty(args[0].shape)
-    max_add_graph_op(result, args[0], args[1])
-    return [result]
 
 def my_compiler(gm: torch.fx.GraphModule, example_inputs: list[torch.Tensor]):
     gm.graph.print_tabular()
-    return torch_add_with_max  # return a python callable
+
+    def max_add_i_want_to_use(*args):
+        return sum(args)
+
+    op = MyMaxOp(
+        max_add_i_want_to_use,
+        max_add_i_want_to_use.__name__,
+        CustomOpLibrary(KernelLibrary(mlir.Context())),
+        input_types=None,
+        output_types=None,
+        num_outputs=1,
+        num_inputs=len(example_inputs)
+    )
+    custom_op_def = op.custom_op_def()
+
+    def torch_add_with_max(*args) -> torch.Tensor:
+        result = args[0].new_empty(args[0].shape)
+        custom_op_def(result, *args)
+        return [result]
+
+    return torch_add_with_max
+
 
 @torch.compile(backend=my_compiler)
 def fn(x, y):
     return x + y
+
 
 a = torch.randn(3)
 print(a)
